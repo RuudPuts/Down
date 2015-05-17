9898da5b1cc0bd46a021b16f0f9edf6160415bc9
Ruud Puts <ruud.puts@gmail.com>
2015-05-17 15:36:58 +0200

    Updated UIView constraint extension to match UIView+Constraints

--- Down/Down/Classes/Extensions/UIView+Extensions.swift
+++ Down/Down/Classes/Extensions/UIView+Extensions.swift
@@ -17,48 +17,85 @@ extension UIView {
     
     // MARK: - Constraints
     
+    var leftConstraint: NSLayoutConstraint? {
+        get {
+            return findParentConstraint(.Leading)
+        }
+    }
+    
+    
+    var rightConstraint: NSLayoutConstraint? {
+        get {
+            return findParentConstraint(.Trailing)
+        }
+    }
+    
+    
+    var topConstraint: NSLayoutConstraint? {
+        get {
+            return findParentConstraint(.Top)
+        }
+    }
+    
+    var bottomConstraint: NSLayoutConstraint? {
+        get {
+            return findParentConstraint(.Bottom)
+        }
+    }
+    
+    // MARK: - Constraints (Width/Height)
+    
     var widthConstraint: NSLayoutConstraint? {
         get {
-            var widthConstraint: NSLayoutConstraint?
-            
-            for (index, constraint: NSLayoutConstraint) in enumerate(self.constraints() as! [NSLayoutConstraint]) {
-                if constraint.firstAttribute == .Width && constraint.isMemberOfClass(NSLayoutConstraint) {
-                    widthConstraint = constraint
-                }
-            }
-            
-            return widthConstraint
+            return findConstraint(.Width)
         }
     }
     
     var heightConstraint: NSLayoutConstraint? {
         get {
-            var heightConstraint: NSLayoutConstraint?
-            
-            for (index, constraint: NSLayoutConstraint) in enumerate(self.constraints() as! [NSLayoutConstraint]) {
-                if constraint.firstAttribute == .Height && constraint.isMemberOfClass(NSLayoutConstraint) {
-                    heightConstraint = constraint
-                }
-            }
-
-            return heightConstraint
+            return findConstraint(.Height)
         }
     }
+    
+    private func findConstraint(attribute: NSLayoutAttribute) -> NSLayoutConstraint? {
+        var foundConstraint: NSLayoutConstraint?
+        
+        if attribute == .Width || attribute == .Height {
+            for (index, constraint: NSLayoutConstraint) in enumerate(self.constraints() as! [NSLayoutConstraint]) {
+                if constraint.isMemberOfClass(NSLayoutConstraint) && constraint.firstAttribute == attribute {
+                    foundConstraint = constraint
+                }
+            }
+        }
+        
+        return foundConstraint
+    }
+    
+    // MARK: - Constraints (Center)
     
     var horizontalCenterConstraint: NSLayoutConstraint? {
         get {
-            var centerConstraint: NSLayoutConstraint?
-            
-            for (index, constraint: NSLayoutConstraint) in enumerate(self.superview?.constraints() as! [NSLayoutConstraint]) {
-                if ((constraint.firstItem as! UIView == self && constraint.firstAttribute == .CenterX) ||
-                    (constraint.secondItem as! UIView? == self && constraint.secondAttribute == .CenterX)) &&
-                    constraint.isMemberOfClass(NSLayoutConstraint) {
-                    centerConstraint = constraint
-                }
-            }
-            
-            return centerConstraint
+            return findParentConstraint(.CenterX)
         }
     }
     
+    var verticalCenterConstraint: NSLayoutConstraint? {
+        get {
+            return findParentConstraint(.CenterY)
+        }
+    }
+    
+    private func findParentConstraint(attribute: NSLayoutAttribute) -> NSLayoutConstraint? {
+        var foundConstraint: NSLayoutConstraint?
+        
+        for (index, constraint: NSLayoutConstraint) in enumerate(self.superview?.constraints() as! [NSLayoutConstraint]) {
+            if ((constraint.firstItem as! UIView == self && constraint.firstAttribute == attribute) ||
+                (constraint.secondItem as! UIView? == self && constraint.secondAttribute == attribute)) &&
+                constraint.isMemberOfClass(NSLayoutConstraint) {
+                    foundConstraint = constraint
+            }
+        }
+        
+        return foundConstraint
+    }
 }
