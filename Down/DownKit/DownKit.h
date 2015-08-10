3bc3f2cbf8b2dfd1069e8bed788a36b99c94e6d5
Ruud Puts <ruud.puts@gmail.com>
2015-08-10 22:11:04 +0200

    Added DownKit framework

---
+++ Down/DownKit/DownKit.h
@@ -0,0 +1,19 @@
+//
+//  DownKit.h
+//  DownKit
+//
+//  Created by Ruud Puts on 10/08/15.
+//  Copyright © 2015 Ruud Puts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DownKit.
+FOUNDATION_EXPORT double DownKitVersionNumber;
+
+//! Project version string for DownKit.
+FOUNDATION_EXPORT const unsigned char DownKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DownKit/PublicHeader.h>
+
+
