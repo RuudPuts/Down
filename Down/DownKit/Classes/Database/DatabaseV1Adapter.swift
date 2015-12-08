7908e2359b182ec8b3a8f93e6749d703a6cdb5a5
Ruud Puts <ruud.puts@gmail.com>
2015-12-08 20:30:12 +0100

    Cleanup & hopefully made V1 adapter thread safe

--- Down/DownKit/Classes/Database/DatabaseV1Adapter.swift
+++ Down/DownKit/Classes/Database/DatabaseV1Adapter.swift
@@ -12,29 +12,31 @@ import RealmSwift
 class DatabaseV1Adapter: DatabaseAdapter {
     
     var version = 1
-    var realm: Realm
-//    var database: Realm?
     
-    init() {
-        realm = try! Realm(path: DatabaseManager.databasePath)
+    func defaultRealm() -> Realm {
+        return try! Realm(path: DatabaseManager.databasePath)
     }
 
     // MARK: Shows
     
     func storeSickbeardShows(shows: [SickbeardShow]) {
+        let realm = defaultRealm()
         try! realm.write({
             for show in shows {
-                self.realm.add(show, update: true)
+                realm.add(show, update: true)
                 NSLog("Stored show \(show.name) (\(show.seasons.count) seasons)")
             }
         })
     }
     
     func allSickbeardShows() -> Results<SickbeardShow> {
+        let realm = defaultRealm()
+        
         return realm.objects(SickbeardShow)
     }
     
     func setFilename(filename: String, forEpisode episode: SickbeardEpisode) {
+        let realm = defaultRealm()
         try! realm.write({
             if !episode.filename.containsString(filename) {
                 episode.filename = filename
@@ -44,7 +46,7 @@ class DatabaseV1Adapter: DatabaseAdapter {
     
     func episodeWithFilename(filename: String!) -> SickbeardEpisode? {
         let predicate = NSPredicate(format: "filename BEGINSWITH %@", filename)
-        let episode = realm.objects(SickbeardEpisode).filter(predicate).first
+        let episode = defaultRealm().objects(SickbeardEpisode).filter(predicate).first
         
         return episode
     }
