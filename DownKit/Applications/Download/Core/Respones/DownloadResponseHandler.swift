4100fb26cc10a34e81462c8714c4b813b49fdc96
Ruud Puts <ruud.puts@gmail.com>
2018-06-26 02:51:45 +0200

    Lol

--- DownKit/Applications/Download/Core/Respones/DownloadResponseHandler.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DownloadResponseHandler.swift
-//  Down
-//
-//  Created by Ruud Puts on 22/06/2018
-//  Copyright © 2018 Mobile Sorcery. All rights reserved.
-//
-
-typealias DownloadResponseHandler = ResponseHandler
--- DownKit/Applications/Dvr/Respones/DvrResponseHandler.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DvrResponseHandler.swift
-//  Down
-//
-//  Created by Ruud Puts on 13/01/2018.
-//  Copyright © 2018 Mobile Sorcery. All rights reserved.
-//
-
-typealias DvrResponseHandler = ResponseHandler
