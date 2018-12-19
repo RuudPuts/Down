891d87d8d88eccad6b273f197aa19f9de8bfe3bf
Ruud Puts <ruud.puts@gmail.com>
2018-12-19 18:28:07 +0100

    Made show sorting more logical

--- Down/Dvr/Shows overview/DvrShowsViewModel.swift
+++ Down/Dvr/Shows overview/DvrShowsViewModel.swift
@@ -39,9 +39,27 @@ extension DvrShowsViewModel: ReactiveBindable {
 
         let showsDriver = dependencies.database
             .fetchShows()
-            .map { $0.sorted(by: { $0.name < $1.name }) }
+            .map { $0.sorted(by: { $0.nameWithoutPrefix < $1.nameWithoutPrefix }) }
             .asDriver(onErrorJustReturn: [])
 
         return Output(refreshShowCache: refreshCacheSingle, shows: showsDriver)
+    }
+}
+
+private extension DvrShow {
+    var nameWithoutPrefix: String {
+        return name.removingPrefixes(["a", "the"])
+    }
+}
+
+// From https://stackoverflow.com/a/47469059
+private extension String {
+    func removingPrefixes(_ prefixes: [String]) -> String {
+        let pattern = "^(\(prefixes.map { "\\Q"+$0+"\\E" }.joined(separator: "|")))\\s?"
+        guard let range = self.range(of: pattern, options: [.regularExpression, .caseInsensitive]) else {
+            return self
+        }
+
+        return String(self[range.upperBound...])
     }
 }
