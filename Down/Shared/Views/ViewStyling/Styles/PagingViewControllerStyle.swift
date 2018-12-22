74774587f2eceabbb1ddee3104ab27fa8a6bd4af
Ruud Puts <ruud.puts@gmail.com>
2018-12-22 23:15:33 +0100

    Insets for the pages

--- Down/Shared/Views/ViewStyling/Styles/PagingViewControllerStyle.swift
+++ Down/Shared/Views/ViewStyling/Styles/PagingViewControllerStyle.swift
@@ -16,6 +16,10 @@ extension ViewStyling where ViewType: FixedPagingViewController {
             $0.borderOptions = .hidden
 
             $0.indicatorColor = Stylesheet.Colors.primaryColor(for: applicationType)
+            $0.indicatorOptions = .visible(height: 4,
+                                           zIndex: Int.max,
+                                           spacing: UIEdgeInsets.zero,
+                                           insets: UIEdgeInsets(top: 0, left: 8, bottom: 4, right: 8))
 
             let itemWidth = $0.view.bounds.width / CGFloat($0.children.count)
             $0.menuItemSize = .sizeToFit(minWidth: itemWidth, height: 40)
